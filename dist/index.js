82fdf9011d6c81456499e31493cf55f9f091a66e
Grant Birkinbine <grantbirki@github.com>
2022-09-24 22:43:36 -0600

    fmt mjs

--- dist/index.js
+++ dist/index.js
@@ -13557,7 +13557,7 @@ function loadConfig() {
     return js_yaml.load((0,external_fs_.readFileSync)(process.env.CONFIG_PATH, 'utf8'))
   } catch (e) {
     core.setFailed(e.message)
-    process.exit();
+    process.exit()
   }
 }
 
@@ -13707,7 +13707,7 @@ async function processResults(results) {
       core.warning(`The Auditor found ${results.counter} findings`)
     }
 
-    if (process.env.CI !== "true") {
+    if (process.env.CI !== 'true') {
       console.log('\n', results.message)
     }
   } else {
