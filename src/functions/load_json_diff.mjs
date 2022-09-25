bc4bf02cd634abfa5d27f8561b7c58fbb0bc90b6
Grant Birkinbine <grantbirki@github.com>
2022-09-24 20:34:45 -0600

    warn if empty

--- src/functions/load_json_diff.mjs
+++ src/functions/load_json_diff.mjs
@@ -3,9 +3,19 @@ import {readFileSync} from 'fs'
 
 export function loadJsonDiff() {
   try {
-    return JSON.parse(readFileSync(process.env.JSON_DIFF_PATH));
+    const raw = readFileSync(process.env.JSON_DIFF_PATH)
+
+    // if the file contents are empty, warn and exit
+    if (raw.length === 0) {
+      core.warning(
+        `JSON diff file is empty - file: ${process.env.JSON_DIFF_PATH}`
+      )
+      process.exit(0)
+    }
+
+    return JSON.parse(raw)
   } catch (e) {
     core.setFailed(e.message)
-    process.exit();
+    process.exit()
   }
 }
